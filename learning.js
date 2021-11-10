2aec3fdaedcf94602268b852e1b20d6c1a6dc151
Audrey <audrey.jacks@icloud.com>
2021-11-10 12:15:09 -0500

    learning.js

--- learning.js
+++ learning.js
@@ -1,6 +1,45 @@
-console.log("Hello World!");
+let cl = console.log
+cl("Hello World!" + " Hello World!");
 let name = "Audrey";
-console.log(name + " Jacks");
+cl(name + " Jacks");
 
-console.log("843-471-3030");
-console.log(843-471-3030);
+cl("843-471-3030");
+cl(843-471-3030);
+
+
+for (let count = 1; count < 9; count++) {
+    cl(count);   
+} 
+console.log("--------------------")
+
+let tree = "#"
+for (let index = 0; index < 7; index++) {  tree += "#"
+    console.log(tree);
+}
+console.log ("#")
+
+console.log("--------------------")
+
+for (let index= 0; index < 12; index++)
+{
+    if (index % 2 === 0) { continue;}
+    console.log(index);
+}
+
+console.log("--------------------")
+
+
+   // let name = "Audrey Jacks";
+  //for (let index = 0; index < name.length; index++) {
+   // console.log ("name[index]"); 
+//}
+
+
+//fizzbuzz code:
+
+for (var index = 1; index < 101; index++) {
+    if (index % 15 == 0) console.log("FizzBuzz");
+    else if (index % 3 == 0) console.log("Fizz");
+    else if (index % 5 == 0) console.log("Buzz");
+    else console.log(index);
+}
