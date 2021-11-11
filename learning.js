43631e67c3bc81ba98982c4fe98705957ef18b48
Audrey <audrey.jacks@icloud.com>
2021-11-11 10:02:35 -0500

    fizzbuzz

--- learning.js
+++ learning.js
@@ -38,8 +38,11 @@ console.log("--------------------")
 //fizzbuzz code:
 
 for (var index = 1; index < 101; index++) {
-    if (index % 15 == 0) console.log("FizzBuzz");
-    else if (index % 3 == 0) console.log("Fizz");
-    else if (index % 5 == 0) console.log("Buzz");
+    if (index % 15 == 0) 
+    console.log("FizzBuzz");
+    else if (index % 3 == 0) 
+    console.log("Fizz");
+    else if (index % 5 == 0) 
+    console.log("Buzz");
     else console.log(index);
 }
