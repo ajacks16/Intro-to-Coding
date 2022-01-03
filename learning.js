feac16a1ad20a7aea9b0ba8f09e05d4324881f36
Audrey <audrey.jacks@icloud.com>
2022-01-03 17:05:33 -0500

    there was definetly an easier way to do this

--- learning.js
+++ learning.js
@@ -1,32 +1,32 @@
-let cl = console.log
-cl("Hello World!" + " Hello World!");
-let name = "Audrey";
-cl(name + " Jacks");
+//let cl = console.log
+//cl("Hello World!" + " Hello World!");
+//let name = "Audrey"; 
+//cl(name + " Jacks");
 
-cl("843-471-3030");
-cl(843-471-3030);
+//cl("843-471-3030");
+//cl(843-471-3030);
 
 
-for (let count = 1; count < 9; count++) {
-    cl(count);   
-} 
-console.log("--------------------")
+//for (let count = 1; count < 9; count++) {
+ //   cl(count);   
+//} 
+//console.log("--------------------")
 
-let tree = "#"
-for (let index = 0; index < 7; index++) {  tree += "#"
-    console.log(tree);
-}
-console.log ("#")
+//let tree = "#"
+//for (let index = 0; index < 7; index++) {  tree += "#"
+ //   console.log(tree);
+//}
+//console.log ("#")
 
-console.log("--------------------")
+//console.log("--------------------")
 
-for (let index= 0; index < 12; index++)
-{
-    if (index % 2 === 0) { continue;}
-    console.log(index);
-}
+//for (let index= 0; index < 12; index++)
+//{
+ //   if (index % 2 === 0) { continue;}
+ //   console.log(index);
+//}
 
-console.log("--------------------")
+//console.log("--------------------")
 
 
    // let name = "Audrey Jacks";
@@ -44,63 +44,63 @@ console.log("--------------------")
     //else console.log(index);
 //}
 
-console.log("--------------------------")
-let doSomething = function(){
+//console.log("--------------------------")
+//let doSomething = function(){
     //run this code later
-}
+//}
 
-let fun = function() {
-    console.log("Function says hello");
-};
-fun(); fun(); fun(); fun();
+//let fun = function() {
+ //   console.log("Function says hello");
+//};
+//fun(); fun(); fun(); fun();
 
-console.log("---------------------------")
+//console.log("---------------------------")
 
-let getGreeting = function() {
-    return "Hello From a Function!";
-};
-let greeting = getGreeting();
-console.log(greeting);
+//let getGreeting = function() {
+   // return "Hello From a Function!";
+//};
+//let greeting = getGreeting();
+//console.log(greeting);
 
-console.log("---------------------------")
-let greet = function(name, timeOfDay) {
-    return "Good " + timeOfDay + ", " + name;
-}
-console.log(greet("Bob", "evening"));
-console.log(greet("Quinn", "day"));
+//console.log("---------------------------")
+//let greet = function(name, timeOfDay) {
+    //return "Good " + timeOfDay + ", " + name;
+//}
+//console.log(greet("Bob", "evening"));
+//console.log(greet("Quinn", "day"));
 
-console.log("---------------------------")
+//console.log("---------------------------")
 
-let add = function(num1, num2) {
-    return num1 + num2;
-}
-console.log("Adding")
-console.log(add(5, 10));
+//let add = function(num1, num2) {
+ //   return num1 + num2;
+//}
+//console.log("Adding")
+//console.log(add(5, 10));
 
-let subtract = function(num1, num2) {
-    return num1 - num2;
-}
-console.log("Subtracting")
-console.log(subtract(37298, 263));
+//let subtract = function(num1, num2) {
+  //  return num1 - num2;
+//}
+//console.log("Subtracting")
+//console.log(subtract(37298, 263));
 
-let multiply = function(num1, num2) {
-    return num1 * num2;
-}
-console.log("Multiplication")
-console.log(multiply(84, 12));
+//let multiply = function(num1, num2) {
+ //   return num1 * num2;
+//}
+//console.log("Multiplication")
+//console.log(multiply(84, 12));
 
-let divide = function(num1, num2) {
-    return num1 / num2;
-}
-console.log("Division")
-console.log(divide(76, 3));
+//let divide = function(num1, num2) {
+//    return num1 / num2;
+//}
+//console.log("Division")
+//console.log(divide(76, 3));
 
-console.log("------------------------------")
+//console.log("------------------------------")
 
-let a = [1, 2, 3];
-let b = ["x", "y", "z"];
-let c = [a, b]
-console.log(a, b, c)
+//let a = [1, 2, 3];
+//let b = ["x", "y", "z"];
+//let c = [a, b]
+//console.log(a, b, c)
 
 //push and pop
 //let a = [1, 2, 3];
@@ -117,10 +117,10 @@ console.log(a, b, c)
 //cl( o );
 
 //this is the same thing as this ^
-let o = {
-    name: "Nathan",
-    age: 100
-};
-console.table(o);
+//let o = {
+ //   name: "Nathan",
+ //   age: 100
+//};
+//.table(o);
 
-console.log("------------------------------")
+//console.log("------------------------------")
