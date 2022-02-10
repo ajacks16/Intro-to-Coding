21d41db1752d15f8750c7c159b0a740a49755d8f
Audrey <audrey.jacks@icloud.com>
2022-02-10 10:40:01 -0500

    card games

--- cards.js
+++ cards.js
@@ -48,9 +48,24 @@ let dealCard = function () {let index = Math.floor( Math.random() * deck.length)
 let dealHand = function () {
     return [dealCard(), dealCard(), dealCard(), dealCard(), dealCard()];
 };
+let dealCard2 = function () {let index = Math.floor( Math.random() * deck.length);
+    let card = deck.splice(index, 1) [0];
+    return card;};
+let dealHand2 = function () {
+    return [dealCard(), dealCard(), dealCard(), dealCard(), dealCard()];
+};
 
 let deck = buildDeck();
 console.table(deck);
 
 let hand = dealHand();
 console.table(hand);
+
+let hand2 = dealHand2();
+console.table(hand2);
+
+
+//figure out how to get the stuff in the terminal here
+//use card-game.js for the actual website (https://r.search.yahoo.com/_ylt=AwrC5rJLLgViDlEAhRD8w8QF;_ylu=c2VjA2NkLWF0dHIEc2xrA3NvdXJjZQR2dGlkAwRydXJsA2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9djhhNjNvWHhuYzQ-/RV=2/RE=1644535499/RO=10/RU=https%3a%2f%2fwww.youtube.com%2fwatch%3fv%3dv8a63oXxnc4/RK=2/RS=8xAZseyKlRI76o_KLT03znSr6Dw-)
+
+//for the win-lose situation, figure out how to create a thing that identifies which card has the higher number, then you could probably use the userinput thing from rps.js
